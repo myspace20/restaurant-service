c3c1a0e79b69871bb77838747a23214cfa6abbc1
rogersatsi <rogersatsi98@gmail.com>
2025-07-11 18:29:17 +0000

    feat(exceptions): add exception handling for forbiden access

--- src/main/java/com/bytebites/restaurant_service/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/bytebites/restaurant_service/exceptions/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.bytebites.restaurant_service.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -55,5 +56,12 @@ public class GlobalExceptionHandler {
                         ex.getMessage(),
                         HttpStatus.INTERNAL_SERVER_ERROR.value()
                 ), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<String> handleAccessDeniedException(AccessDeniedException ex) {
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body("Access Denied: You don't have the required permissions.");
     }
 }
