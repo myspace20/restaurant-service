0d0ad5e7a890a64c4e98ba39e1d84ac5da5c7431
rogersatsi <rogersatsi98@gmail.com>
2025-07-11 18:36:34 +0000

    feat: add structured logging, men ownership check and update restaurant ownership check

--- src/main/java/com/bytebites/restaurant_service/services/RestaurantOwnershipServiceImpl.java
+++ src/main/java/com/bytebites/restaurant_service/services/RestaurantOwnershipServiceImpl.java
@@ -1,19 +1,39 @@
 package com.bytebites.restaurant_service.services;
 
+import com.bytebites.restaurant_service.models.Menu;
+import com.bytebites.restaurant_service.models.Restaurant;
+import com.bytebites.restaurant_service.repositories.MenuRepository;
 import com.bytebites.restaurant_service.repositories.RestaurantRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component("ownershipService")
 public class RestaurantOwnershipServiceImpl implements RestaurantOwnershipService {
 
     private final RestaurantRepository restaurantRepository;
+    private final MenuRepository menuRepository;
 
-    public RestaurantOwnershipServiceImpl(RestaurantRepository restaurantRepository) {
+    private final Logger logger = LoggerFactory.getLogger(RestaurantOwnershipServiceImpl.class);
+
+    public RestaurantOwnershipServiceImpl(RestaurantRepository restaurantRepository, MenuRepository menuRepository) {
         this.restaurantRepository = restaurantRepository;
+        this.menuRepository = menuRepository;
     }
 
     @Override
-    public boolean checkRestaurantOwnership(Long ownerId) {
-        return restaurantRepository.existsByOwnerId(ownerId);
+    public boolean checkRestaurantOwnership(Long restaurantId, Long ownerId ) {
+        logger.info("Checking restaurant ownership for ownerId={}, restaurantId={}", ownerId, restaurantId);
+        return restaurantRepository.existsByIdAndOwnerId(restaurantId, ownerId);
+    }
+
+    @Override
+    public boolean checkMenuOwnership(Long menuId, Long ownerId) {
+        logger.info("Checking menu ownership for ownerId={}, menuId={}", ownerId, menuId);
+        return menuRepository.findById(menuId)
+                .map(Menu::getRestaurant)
+                .map(Restaurant::getOwnerId)
+                .map(owner_Id -> owner_Id.equals(ownerId))
+                .orElse(false);
     }
 }
