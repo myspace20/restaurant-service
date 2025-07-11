21c8eabafb2d2723f13351c28dff35c39e161f99
rogersatsi <rogersatsi98@gmail.com>
2025-07-11 18:37:40 +0000

    feat: add structured logging and update method level security check

--- src/main/java/com/bytebites/restaurant_service/services/RestaurantServiceImpl.java
+++ src/main/java/com/bytebites/restaurant_service/services/RestaurantServiceImpl.java
@@ -10,6 +10,8 @@ import com.bytebites.restaurant_service.models.Menu;
 import com.bytebites.restaurant_service.repositories.MenuRepository;
 import com.bytebites.restaurant_service.repositories.RestaurantRepository;
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +25,9 @@ public class RestaurantServiceImpl implements RestaurantService {
     private final MenuRepository menuRepository;
     private final RestaurantMapper restaurantMapper;
 
+    private final Logger logger = LoggerFactory.getLogger(RestaurantServiceImpl.class);
+
+
     public RestaurantServiceImpl(MenuRepository menuRepository, RestaurantRepository restaurantRepository, RestaurantMapper restaurantMapper) {
         this.menuRepository = menuRepository;
         this.restaurantRepository = restaurantRepository;
@@ -31,6 +36,7 @@ public class RestaurantServiceImpl implements RestaurantService {
 
     @Override
     public Restaurant getRestaurantById(Long id){
+        logger.info("Getting restaurant by id {} ", id);
         return restaurantRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFound("Restaurant not found"));
     }
@@ -38,6 +44,7 @@ public class RestaurantServiceImpl implements RestaurantService {
     @Override
     @PreAuthorize("isAuthenticated()")
     public List<RestaurantResponse> getAllRestaurants() {
+        logger.info("Getting all restaurants ");
        return restaurantRepository.findAll().stream()
                .map(restaurantMapper::toRestaurantResponse)
                .collect(Collectors.toList());
@@ -46,6 +53,7 @@ public class RestaurantServiceImpl implements RestaurantService {
     @Override
     @PreAuthorize("isAuthenticated()")
     public RestaurantResponse getRestaurantByName(String restaurantName) {
+        logger.info("Getting restaurant by name {} ", restaurantName);
         Restaurant restaurant = restaurantRepository.findByName(restaurantName)
                 .orElseThrow(()->new ResourceNotFound("Restaurant not found"));
         return restaurantMapper.toRestaurantResponse(restaurant);
@@ -54,6 +62,7 @@ public class RestaurantServiceImpl implements RestaurantService {
     @Override
     @PreAuthorize("hasRole('ROLE_RESTAURANT_OWNER')")
     public RestaurantResponse createRestaurant(RestaurantRequest restaurantRequest) {
+        logger.info("Creating restaurant with {}", restaurantRequest);
         Restaurant restaurant = new Restaurant(
                 restaurantRequest.name()
                 ,restaurantRequest.address(),
@@ -66,8 +75,9 @@ public class RestaurantServiceImpl implements RestaurantService {
     }
 
     @Override
-    @PreAuthorize("hasRole('ROLE_RESTAURANT_OWNER') and ownershipService.checkRestaurantOwnership(authentication.principal.id)")
+    @PreAuthorize("hasRole('ROLE_RESTAURANT_OWNER') and @ownershipService.checkRestaurantOwnership(#id, authentication.principal.id)")
     public void updateRestaurant(Long id, RestaurantRequest restaurantRequest) {
+        logger.info("Updating restaurant with id {} and requestBody {}", id, restaurantRequest);
         Restaurant restaurantToUpdate = getRestaurantById(id);
         Restaurant updatedRestaurant = setRestaurantToUpdateProperties(
                 restaurantToUpdate,
@@ -78,16 +88,18 @@ public class RestaurantServiceImpl implements RestaurantService {
 
     @Override
     @Transactional
-    @PreAuthorize("hasRole('ROLE_RESTAURANT_OWNER') and ownershipService.checkRestaurantOwnership(authentication.principal.id)")
+    @PreAuthorize("hasRole('ROLE_RESTAURANT_OWNER') and @ownershipService.checkRestaurantOwnership(#id, authentication.principal.id)")
     public void deleteRestaurant(Long id) {
+        logger.info("Deleting restaurant with id {} ", id);
         Restaurant  restaurant = getRestaurantById(id);
         restaurantRepository.delete(restaurant);
     }
 
     @Override
     @Transactional
-    @PreAuthorize("hasRole('ROLE_RESTAURANT_OWNER') and ownershipService.checkRestaurantOwnership(authentication.principal.id)")
+    @PreAuthorize("hasRole('ROLE_RESTAURANT_OWNER') and @ownershipService.checkRestaurantOwnership(#restaurantId, authentication.principal.id)")
     public void removeMenuFromRestaurant(Long restaurantId, Long menuId) {
+        logger.info("Removing menu from restaurant with id {} and menu id {}", restaurantId, menuId);
         Restaurant restaurant = getRestaurantById(restaurantId);
         Menu menu = getMenuById(menuId);
         restaurant.removeMenu(menu);
@@ -96,6 +108,7 @@ public class RestaurantServiceImpl implements RestaurantService {
 
     @Override
     public RestaurantResponse getRestaurantByOwnerId(Long ownerId) {
+        logger.info("Getting restaurant by owner id {} ", ownerId);
         Restaurant restaurant = restaurantRepository.findByOwnerId(ownerId)
                 .orElseThrow(() -> new ResourceNotFound("Restaurant not found"));
         return restaurantMapper.toRestaurantResponse(restaurant);
