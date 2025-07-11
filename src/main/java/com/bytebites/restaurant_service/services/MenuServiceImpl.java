8be6c732435e8b8a698892c4011937e84cc4e57c
rogersatsi <rogersatsi98@gmail.com>
2025-07-11 18:33:38 +0000

    feat: add logging, additional error handling and update roles and ownership checks

--- src/main/java/com/bytebites/restaurant_service/services/MenuServiceImpl.java
+++ src/main/java/com/bytebites/restaurant_service/services/MenuServiceImpl.java
@@ -2,11 +2,14 @@ package com.bytebites.restaurant_service.services;
 
 import com.bytebites.restaurant_service.dto.MenuRequest;
 import com.bytebites.restaurant_service.dto.MenuResponse;
+import com.bytebites.restaurant_service.exceptions.ResourceNotFound;
 import com.bytebites.restaurant_service.mappers.MenuMapper;
 import com.bytebites.restaurant_service.models.Menu;
 import com.bytebites.restaurant_service.models.Restaurant;
 import com.bytebites.restaurant_service.repositories.MenuRepository;
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +21,8 @@ public class MenuServiceImpl implements MenuService {
     private final MenuMapper menuMapper;
     private final RestaurantService restaurantService;
 
+    private final Logger logger = LoggerFactory.getLogger(MenuServiceImpl.class);
+
 
     public MenuServiceImpl(MenuRepository menuRepository, MenuMapper menuMapper, RestaurantService restaurantService) {
         this.menuRepository = menuRepository;
@@ -27,14 +32,16 @@ public class MenuServiceImpl implements MenuService {
 
     @Override
     public MenuResponse findMenuByName(String name) {
+        logger.info("Find menu by name {} ", name);
         Menu menu = menuRepository.findByName(name)
-                .orElseThrow(()->new RuntimeException("Menu not found"));
+                .orElseThrow(()->new ResourceNotFound("Menu not found"));
         return menuMapper.toMenuResponse(menu);
     }
 
     @Override
     @PreAuthorize("hasRole('ROLE_RESTAURANT_OWNER')")
     public MenuResponse createMenu(MenuRequest menuRequest) {
+        logger.info("Creating new menu {}", menuRequest);
        Restaurant restaurant = restaurantService
                .getRestaurantById(menuRequest.restaurant_id());
         Menu menu = new Menu(
@@ -49,8 +56,9 @@ public class MenuServiceImpl implements MenuService {
 
     @Override
     @Transactional
-    @PreAuthorize("hasRole('ROLE_RESTAURANT_OWNER') and ownershipService.checkRestaurantOwnership(authentication.principal.id)")
+    @PreAuthorize("hasRole('ROLE_RESTAURANT_OWNER') and @ownershipService.checkMenuOwnership(#id,authentication.principal.id)")
     public void updateMenu(Long id, MenuRequest menuRequest) {
+        logger.info("Updating menu with id {} and request body {}", id, menuRequest );
        Menu menuToUpdate = findMenuById(id);
        Menu updatedMenu = setMenuToUpdateAttributes(
                menuToUpdate,
@@ -61,8 +69,9 @@ public class MenuServiceImpl implements MenuService {
 
     @Override
     @Transactional
-    @PreAuthorize("hasRole('ROLE_RESTAURANT_OWNER') and ownershipService.checkRestaurantOwnership(authentication.principal.id)")
+    @PreAuthorize("hasRole('ROLE_RESTAURANT_OWNER') and @ownershipService.checkMenuOwnership(#id,authentication.principal.id)")
     public void deleteMenu(Long id) {
+        logger.info("Deleting menu with id {}", id);
         Menu menuToDelete = findMenuById(id);
         menuRepository.delete(menuToDelete);
     }
@@ -76,6 +85,6 @@ public class MenuServiceImpl implements MenuService {
 
     private Menu findMenuById(Long id) {
         return menuRepository.findById(id)
-                .orElseThrow(()->new RuntimeException("Menu not found"));
+                .orElseThrow(()->new ResourceNotFound("Menu not found"));
     }
 }
